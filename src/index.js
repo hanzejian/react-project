f4bc1c61366e652f804ec8c34749637fbb394e56
hanzejian <hanzejian1998@163.com>
2023-07-22 16:24:47 +0800

    第一个React应用程序

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+/**
+ * 从react的包当中引入了React，只要你写Reat.js组件就必须引入React
+ * 因为React里有一种语法叫 jsx ，要写JSX，就必须引入React
+ */
+import React from 'react'
+/**
+ * ReactDOM里有一个render方法，功能就是把组件渲染并构造DOM树，
+ * 然后插入到页面上某个特定的元素上
+ */
+import ReactDOM from 'react-dom'
+ReactDOM.render(
+  // jsx = js + xml
+  <h1>欢迎来到React的世界！！！</h1>,
+  document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
