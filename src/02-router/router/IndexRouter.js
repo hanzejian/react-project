1eece9cc94f4cfdb2ba1705287d1ddbaf1349bbb
hanzejian <hanzejian1998@163.com>
2023-07-28 07:46:30 +0800

    02-路由重定向

--- src/02-router/router/IndexRouter.js
+++ src/02-router/router/IndexRouter.js
@@ -1,16 +1,26 @@
 import React, { Component } from 'react'
-import { HashRouter, Route } from 'react-router-dom'
+import { HashRouter, Route, Redirect, Switch } from 'react-router-dom'
 import Films from '../views/Films'
 import Cinemas from '../views/Cinemas'
 import Center from '../views/Center'
+import NotFound from '../views/NotFound'
 
 export default class IndexRouter extends Component {
   render() {
     return (
       <HashRouter>
-          <Route path="/films" component={Films}></Route>
-          <Route path="/cinemas" component={Cinemas}></Route>
-          <Route path="/center" component={Center}></Route>
+          <Switch>
+            <Route path="/films" component={Films}></Route>
+            <Route path="/cinemas" component={Cinemas}></Route>
+            <Route path="/center" component={Center}></Route>
+
+            {/* 重定向，模糊匹配 */}
+            {/* <Redirect from="/" to="/films"></Redirect> */}
+
+            {/* 重定向，exact精准匹配 */}
+            <Redirect from="/" to="/films" exact></Redirect>
+            <Route component={NotFound}></Route>
+          </Switch>
       </HashRouter>
     )
   }
