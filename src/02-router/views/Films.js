1c67e915b0434452619ae78a07377dc96e5b7318
hanzejian <hanzejian1998@163.com>
2023-07-29 10:00:16 +0800

    02-二级路由嵌套

--- src/02-router/views/Films.js
+++ src/02-router/views/Films.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react'
-
+import { Route, Redirect } from 'react-router-dom'
+import Nowplaying from './films/Nowplaying'
+import ComingSoon from './films/ComingSoon'
 export default class Films extends Component {
   render() {
     return (
-      <div>Films</div>
+      <div>
+        <div style={{ height: '200px', background: 'yellow' }}>大轮播</div>
+        <div>导航栏</div>
+        {/* 路由配置 嵌套路由 */}
+        <Route path="/films/nowplaying" component={Nowplaying}></Route>
+        <Route path="/films/comingsoon" component={ComingSoon}></Route>
+        <Redirect from="films" to="/films/nowplaying"></Redirect>
+      </div>
     )
   }
 }
